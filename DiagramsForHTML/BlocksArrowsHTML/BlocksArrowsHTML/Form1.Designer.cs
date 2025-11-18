ef76aa97f2b6cc5d79e0755ed368dd2cc0a114b1
Murilo Fujita <murilofujita@gmail.com>
2025-11-18 14:52:03 -0300

    Version to test the drawing

---
+++ DiagramsForHTML/BlocksArrowsHTML/BlocksArrowsHTML/Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace BlocksArrowsHTML
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblText1 = new System.Windows.Forms.Label();
+            this.txtText1 = new System.Windows.Forms.TextBox();
+            this.txtText2 = new System.Windows.Forms.TextBox();
+            this.lblTexto2 = new System.Windows.Forms.Label();
+            this.txtText3 = new System.Windows.Forms.TextBox();
+            this.lblText3 = new System.Windows.Forms.Label();
+            this.txtText4 = new System.Windows.Forms.TextBox();
+            this.lblText4 = new System.Windows.Forms.Label();
+            this.txtText5 = new System.Windows.Forms.TextBox();
+            this.lblText5 = new System.Windows.Forms.Label();
+            this.txtText6 = new System.Windows.Forms.TextBox();
+            this.lblText6 = new System.Windows.Forms.Label();
+            this.BtnMakeCode = new System.Windows.Forms.Button();
+            this.lblInicioEsquerda = new System.Windows.Forms.Label();
+            this.lblInicioSuperior = new System.Windows.Forms.Label();
+            this.txtInicioEsquerda = new System.Windows.Forms.TextBox();
+            this.txtInicioSuperior = new System.Windows.Forms.TextBox();
+            this.lblComprimento = new System.Windows.Forms.Label();
+            this.txtComprimento = new System.Windows.Forms.TextBox();
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.lblDistanciaRetangulos = new System.Windows.Forms.Label();
+            this.txtDistanciaRetangulos = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblText1
+            // 
+            this.lblText1.AutoSize = true;
+            this.lblText1.Location = new System.Drawing.Point(13, 21);
+            this.lblText1.Name = "lblText1";
+            this.lblText1.Size = new System.Drawing.Size(71, 20);
+            this.lblText1.TabIndex = 0;
+            this.lblText1.Text = "Texto 1";
+            // 
+            // txtText1
+            // 
+            this.txtText1.Location = new System.Drawing.Point(90, 16);
+            this.txtText1.Name = "txtText1";
+            this.txtText1.Size = new System.Drawing.Size(391, 28);
+            this.txtText1.TabIndex = 1;
+            // 
+            // txtText2
+            // 
+            this.txtText2.Location = new System.Drawing.Point(90, 50);
+            this.txtText2.Name = "txtText2";
+            this.txtText2.Size = new System.Drawing.Size(391, 28);
+            this.txtText2.TabIndex = 3;
+            // 
+            // lblTexto2
+            // 
+            this.lblTexto2.AutoSize = true;
+            this.lblTexto2.Location = new System.Drawing.Point(13, 55);
+            this.lblTexto2.Name = "lblTexto2";
+            this.lblTexto2.Size = new System.Drawing.Size(71, 20);
+            this.lblTexto2.TabIndex = 2;
+            this.lblTexto2.Text = "Texto 2";
+            // 
+            // txtText3
+            // 
+            this.txtText3.Location = new System.Drawing.Point(90, 84);
+            this.txtText3.Name = "txtText3";
+            this.txtText3.Size = new System.Drawing.Size(391, 28);
+            this.txtText3.TabIndex = 5;
+            // 
+            // lblText3
+            // 
+            this.lblText3.AutoSize = true;
+            this.lblText3.Location = new System.Drawing.Point(13, 89);
+            this.lblText3.Name = "lblText3";
+            this.lblText3.Size = new System.Drawing.Size(71, 20);
+            this.lblText3.TabIndex = 4;
+            this.lblText3.Text = "Texto 3";
+            // 
+            // txtText4
+            // 
+            this.txtText4.Location = new System.Drawing.Point(89, 119);
+            this.txtText4.Name = "txtText4";
+            this.txtText4.Size = new System.Drawing.Size(391, 28);
+            this.txtText4.TabIndex = 7;
+            // 
+            // lblText4
+            // 
+            this.lblText4.AutoSize = true;
+            this.lblText4.Location = new System.Drawing.Point(12, 124);
+            this.lblText4.Name = "lblText4";
+            this.lblText4.Size = new System.Drawing.Size(71, 20);
+            this.lblText4.TabIndex = 6;
+            this.lblText4.Text = "Texto 4";
+            // 
+            // txtText5
+            // 
+            this.txtText5.Location = new System.Drawing.Point(89, 153);
+            this.txtText5.Name = "txtText5";
+            this.txtText5.Size = new System.Drawing.Size(391, 28);
+            this.txtText5.TabIndex = 9;
+            // 
+            // lblText5
+            // 
+            this.lblText5.AutoSize = true;
+            this.lblText5.Location = new System.Drawing.Point(12, 158);
+            this.lblText5.Name = "lblText5";
+            this.lblText5.Size = new System.Drawing.Size(71, 20);
+            this.lblText5.TabIndex = 8;
+            this.lblText5.Text = "Texto 5";
+            // 
+            // txtText6
+            // 
+            this.txtText6.Location = new System.Drawing.Point(90, 190);
+            this.txtText6.Name = "txtText6";
+            this.txtText6.Size = new System.Drawing.Size(391, 28);
+            this.txtText6.TabIndex = 11;
+            // 
+            // lblText6
+            // 
+            this.lblText6.AutoSize = true;
+            this.lblText6.Location = new System.Drawing.Point(13, 195);
+            this.lblText6.Name = "lblText6";
+            this.lblText6.Size = new System.Drawing.Size(71, 20);
+            this.lblText6.TabIndex = 10;
+            this.lblText6.Text = "Texto 6";
+            // 
+            // BtnMakeCode
+            // 
+            this.BtnMakeCode.Location = new System.Drawing.Point(195, 438);
+            this.BtnMakeCode.Name = "BtnMakeCode";
+            this.BtnMakeCode.Size = new System.Drawing.Size(117, 33);
+            this.BtnMakeCode.TabIndex = 12;
+            this.BtnMakeCode.Text = "Make Code";
+            this.BtnMakeCode.UseVisualStyleBackColor = true;
+            this.BtnMakeCode.Click += new System.EventHandler(this.BtnMakeCode_Click);
+            // 
+            // lblInicioEsquerda
+            // 
+            this.lblInicioEsquerda.AutoSize = true;
+            this.lblInicioEsquerda.Location = new System.Drawing.Point(14, 229);
+            this.lblInicioEsquerda.Name = "lblInicioEsquerda";
+            this.lblInicioEsquerda.Size = new System.Drawing.Size(142, 20);
+            this.lblInicioEsquerda.TabIndex = 13;
+            this.lblInicioEsquerda.Text = "Início esquerda";
+            // 
+            // lblInicioSuperior
+            // 
+            this.lblInicioSuperior.AutoSize = true;
+            this.lblInicioSuperior.Location = new System.Drawing.Point(253, 229);
+            this.lblInicioSuperior.Name = "lblInicioSuperior";
+            this.lblInicioSuperior.Size = new System.Drawing.Size(133, 20);
+            this.lblInicioSuperior.TabIndex = 14;
+            this.lblInicioSuperior.Text = "Início superior";
+            // 
+            // txtInicioEsquerda
+            // 
+            this.txtInicioEsquerda.Location = new System.Drawing.Point(159, 226);
+            this.txtInicioEsquerda.Name = "txtInicioEsquerda";
+            this.txtInicioEsquerda.Size = new System.Drawing.Size(91, 28);
+            this.txtInicioEsquerda.TabIndex = 15;
+            // 
+            // txtInicioSuperior
+            // 
+            this.txtInicioSuperior.Location = new System.Drawing.Point(390, 224);
+            this.txtInicioSuperior.Name = "txtInicioSuperior";
+            this.txtInicioSuperior.Size = new System.Drawing.Size(91, 28);
+            this.txtInicioSuperior.TabIndex = 16;
+            // 
+            // lblComprimento
+            // 
+            this.lblComprimento.AutoSize = true;
+            this.lblComprimento.Location = new System.Drawing.Point(14, 264);
+            this.lblComprimento.Name = "lblComprimento";
+            this.lblComprimento.Size = new System.Drawing.Size(214, 20);
+            this.lblComprimento.TabIndex = 17;
+            this.lblComprimento.Text = "Comprimento retângulo";
+            // 
+            // txtComprimento
+            // 
+            this.txtComprimento.Location = new System.Drawing.Point(228, 259);
+            this.txtComprimento.Name = "txtComprimento";
+            this.txtComprimento.Size = new System.Drawing.Size(58, 28);
+            this.txtComprimento.TabIndex = 18;
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.AutoSize = true;
+            this.lblAltura.Location = new System.Drawing.Point(14, 292);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(149, 20);
+            this.lblAltura.TabIndex = 19;
+            this.lblAltura.Text = "Altura retângulo";
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(166, 289);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(58, 28);
+            this.txtAltura.TabIndex = 20;
+            // 
+            // lblDistanciaRetangulos
+            // 
+            this.lblDistanciaRetangulos.AutoSize = true;
+            this.lblDistanciaRetangulos.Location = new System.Drawing.Point(14, 323);
+            this.lblDistanciaRetangulos.Name = "lblDistanciaRetangulos";
+            this.lblDistanciaRetangulos.Size = new System.Drawing.Size(227, 20);
+            this.lblDistanciaRetangulos.TabIndex = 21;
+            this.lblDistanciaRetangulos.Text = "Distância entre retângulo";
+            // 
+            // txtDistanciaRetangulos
+            // 
+            this.txtDistanciaRetangulos.Location = new System.Drawing.Point(247, 320);
+            this.txtDistanciaRetangulos.Name = "txtDistanciaRetangulos";
+            this.txtDistanciaRetangulos.Size = new System.Drawing.Size(58, 28);
+            this.txtDistanciaRetangulos.TabIndex = 22;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(532, 496);
+            this.Controls.Add(this.txtDistanciaRetangulos);
+            this.Controls.Add(this.lblDistanciaRetangulos);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.lblAltura);
+            this.Controls.Add(this.txtComprimento);
+            this.Controls.Add(this.lblComprimento);
+            this.Controls.Add(this.txtInicioSuperior);
+            this.Controls.Add(this.txtInicioEsquerda);
+            this.Controls.Add(this.lblInicioSuperior);
+            this.Controls.Add(this.lblInicioEsquerda);
+            this.Controls.Add(this.BtnMakeCode);
+            this.Controls.Add(this.txtText6);
+            this.Controls.Add(this.lblText6);
+            this.Controls.Add(this.txtText5);
+            this.Controls.Add(this.lblText5);
+            this.Controls.Add(this.txtText4);
+            this.Controls.Add(this.lblText4);
+            this.Controls.Add(this.txtText3);
+            this.Controls.Add(this.lblText3);
+            this.Controls.Add(this.txtText2);
+            this.Controls.Add(this.lblTexto2);
+            this.Controls.Add(this.txtText1);
+            this.Controls.Add(this.lblText1);
+            this.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Diagram Maker For Canvas HTML";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblText1;
+        private System.Windows.Forms.TextBox txtText1;
+        private System.Windows.Forms.TextBox txtText2;
+        private System.Windows.Forms.Label lblTexto2;
+        private System.Windows.Forms.TextBox txtText3;
+        private System.Windows.Forms.Label lblText3;
+        private System.Windows.Forms.TextBox txtText4;
+        private System.Windows.Forms.Label lblText4;
+        private System.Windows.Forms.TextBox txtText5;
+        private System.Windows.Forms.Label lblText5;
+        private System.Windows.Forms.TextBox txtText6;
+        private System.Windows.Forms.Label lblText6;
+        private System.Windows.Forms.Button BtnMakeCode;
+        private System.Windows.Forms.Label lblInicioEsquerda;
+        private System.Windows.Forms.Label lblInicioSuperior;
+        private System.Windows.Forms.TextBox txtInicioEsquerda;
+        private System.Windows.Forms.TextBox txtInicioSuperior;
+        private System.Windows.Forms.Label lblComprimento;
+        private System.Windows.Forms.TextBox txtComprimento;
+        private System.Windows.Forms.Label lblAltura;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.Label lblDistanciaRetangulos;
+        private System.Windows.Forms.TextBox txtDistanciaRetangulos;
+    }
+}
+
---
+++ DiagramsForHTML/BlocksArrowsHTML/BlocksArrowsHTML/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BlocksArrowsHTML
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Location = new Point(0, 0);
+            txtText1.Text = "Estender toalha de mesa";
+            txtText2.Text = "Trazer pratos e talheres";
+            txtText3.Text = "Temperar salada";
+            txtText4.Text = "Esquentar comida";
+            txtText5.Text = "Fazer a bebida";
+            txtText6.Text = "Iniciar refeição";
+            txtInicioEsquerda.Text = "10";
+            txtInicioSuperior.Text = "20";
+            txtComprimento.Text = "250";
+            txtAltura.Text = "20";
+            txtDistanciaRetangulos.Text = "100";
+        }
+
+        private void BtnMakeCode_Click(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream("canvas.html", FileMode.Create);
+            StreamWriter sw = new StreamWriter(fs);
+            WriteHeader(sw);
+            WriteCode(sw);
+            WriteEndOfFile(sw);
+            sw.Close();
+            fs.Close();
+        }
+
+        private void WriteHeader(StreamWriter sw)
+        {
+            sw.WriteLine("<html lang=\"pt-br\">");
+            sw.WriteLine("<head>");
+            sw.WriteLine("    <meta charset=\"UTF-8\">");
+            sw.WriteLine("    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
+            sw.WriteLine("    <title>Document</title>");
+            sw.WriteLine("</head>");
+            sw.WriteLine("<body>");
+            sw.WriteLine("    <canvas id=\"figura1\" width=\"320px\" height=\"1000px\"></canvas>");
+            sw.WriteLine("	");
+            sw.WriteLine("  <script>");
+        }
+
+        private void WriteCode(StreamWriter sw)
+        {
+            int x = Convert.ToInt32(txtInicioEsquerda.Text);
+            int y = Convert.ToInt32(txtInicioSuperior.Text);
+            int comprimento = Convert.ToInt32(txtComprimento.Text);
+            int altura = Convert.ToInt32(txtAltura.Text);
+            int distancia = Convert.ToInt32(txtDistanciaRetangulos.Text);
+
+            sw.WriteLine("let canvas = document.getElementById('figura1');");
+            sw.WriteLine("let ctx = canvas.getContext('2d');");
+
+            sw.WriteLine("ctx.font=\"16px Arial\";");
+            sw.WriteLine($"ctx.fillText('{txtText1.Text}',{x},{y+ 0*distancia});");
+
+            sw.WriteLine($"ctx.rect({x-2},{y+0*distancia-(int)(altura*.7)},{comprimento},{altura});");
+            sw.WriteLine("ctx.stroke();");
+
+            sw.WriteLine($"ctx.fillText('{txtText2.Text}', {x}, {y + 1*distancia});");
+
+            sw.WriteLine($"ctx.rect({x - 2},{y + 1*distancia - (int)(altura * .7)},{comprimento},{altura});");
+            sw.WriteLine("ctx.stroke();");
+
+            sw.WriteLine($"ctx.fillText('{txtText3.Text}', {x}, {y + 2*distancia});");
+
+            sw.WriteLine($"ctx.rect({x - 2},{y + 2*distancia - (int)(altura * .7)},{comprimento},{altura});");
+            sw.WriteLine("ctx.stroke();");
+
+            sw.WriteLine($"ctx.fillText('{txtText4.Text}', {x}, {y + 3 * distancia});");
+
+            sw.WriteLine($"ctx.rect({x - 2},{y + 3 * distancia - (int)(altura * .7)},{comprimento},{altura});");
+            sw.WriteLine("ctx.stroke();");
+
+            sw.WriteLine($"ctx.fillText('{txtText5.Text}', {x}, {y + 4 * distancia});");
+
+            sw.WriteLine($"ctx.rect({x - 2},{y + 4 * distancia - (int)(altura * .7)},{comprimento},{altura});");
+            sw.WriteLine("ctx.stroke();");
+
+            sw.WriteLine($"ctx.fillText('{txtText6.Text}', {x}, {y + 5 * distancia});");
+
+            sw.WriteLine($"ctx.rect({x - 2},{y + 5 * distancia - (int)(altura * .7)},{comprimento},{altura});");
+            sw.WriteLine("ctx.stroke();");
+
+        }
+
+        private void WriteEndOfFile(StreamWriter sw)
+        {
+            sw.WriteLine("	</script>");
+            sw.WriteLine("</body>");
+            sw.WriteLine("</html>");
+        }
+    }
+}
